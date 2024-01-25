f33f597b4732f2a6e19890b080fabbc50aa09c21
truongmainguyetnhi <tmnnhi-cntt16@tdu.edu.vn>
2024-01-25 10:41:45 +0700

    edit login

--- administrator/userLogin.php
+++ administrator/userLogin.php
@@ -12,25 +12,6 @@
 </head>
 
 <body class="loginbody">
-<<<<<<< HEAD
-    <div align="center" id="logincenter">
-        <h4>ĐĂNG NHẬP HỆ THỐNG</h4>
-        <form name="frmLogin" method="post" action="elements_TMNN/mUser/userAct.php?reqact=checklogin">
-            <table class="father">
-                <tr>
-                    <td>Tên tài khoản: </td>
-                    <td><input type="text" name="username" id="username" /></td>
-                </tr>
-                <tr>
-                    <td>Mật khẩu: </td>
-                    <td><input type="password" name="password" id="password" /></td>
-                </tr>
-                <tr>
-                    <td><br></td>
-                </tr>
-            </table>
-            <input id="loginut" type="submit" value="Đăng nhập" />
-=======
     <div id="logincenter">
         <form class="form_box" name="frmLogin" method="post" action="elements_TMNN/mUser/userAct.php?reqact=checklogin">
             <h1>Sign in</h1>
@@ -43,7 +24,6 @@
                 <label>Mật khẩu</label>
             </div>
             <button type="submit" class="btnut">Đăng nhập</button>
->>>>>>> eb4ee24222af7d896f423565faaa99485be4dc5b
         </form>
     </div>
 </body>
